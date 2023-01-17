9ddd1d1330ccb730b898ff0becdd5af273788c46
VinlaRose <109898811+VinlaRose@users.noreply.github.com>
2023-01-17 17:20:05 +0530

    Update login.js

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,23 +1,9 @@
-// import logo from "./logo.svg";
-import "@aws-amplify/ui-react/styles.css";
-import {
-//   withAuthenticator,
-  Button,
-  Heading,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+export const LoginApp = () => {
 
-export const LoginApp = (signOut) =>{
-  return (
-    <View className="App">
-      <Card>
-        {/* <Image src={logo} className="App-logo" alt="logo" /> */}
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
-      <Button onClick={signOut}>Sign Out</Button>
-    </View>
-    
-  );
+   
+
+    return (
+        <h1>login here.. </h1>
+        
+    )
 }
-
